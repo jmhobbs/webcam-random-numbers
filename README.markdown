274367ab567bb6a875fba9530b173255a9e1d499
John Hobbs <john@velvetcache.org>
2010-03-11 23:13:45 -0600

    Better this time.

---
+++ README.markdown
@@ -0,0 +1,7 @@
+# What is it?
+
+1. Scrape the glass off of a webcam.
+2. Rip apart a smoke alarm.
+3. Smush them together.
+4. Use python to generate random numbers with the radiation.
+5. Make friends and share your data!
--- generator.py
+++ generator.py
@@ -1,7 +1,30 @@
 # -*- coding: utf-8 -*-                               
 
-from opencv import highgui
-from opencv import cv
+#  Copyright (c) 2010 John Hobbs
+#  
+#  Permission is hereby granted, free of charge, to any person obtaining a copy
+#  of this software and associated documentation files (the "Software"), to deal
+#  in the Software without restriction, including without limitation the rights
+#  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#  copies of the Software, and to permit persons to whom the Software is
+#  furnished to do so, subject to the following conditions:
+#  
+#  The above copyright notice and this permission notice shall be included in
+#  all copies or substantial portions of the Software.
+#  
+#  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+#  THE SOFTWARE.
+
+try:
+	from ctypes_opencv import *
+except:
+	from opencv import highgui
+	from opencv import cv
 
 from optparse import OptionParser
 
@@ -11,6 +34,7 @@ if __name__ == "__main__" :
 
 	parser = OptionParser()
 	parser.add_option( "-g", "--gui", action="store_true", dest="showgui", default=False, help="show the gui; slows things down considerably")
+	parser.add_option( "-t", "--threshold", type="int", dest="threshold", default=200, help="threshold over which pixel values must be" )
 	(options, args) = parser.parse_args()
 
 	if options.showgui:
@@ -24,7 +48,8 @@ if __name__ == "__main__" :
 	
 	highgui.cvSetCaptureProperty( camera, highgui.CV_CAP_PROP_FRAME_WIDTH, 640 )
 	highgui.cvSetCaptureProperty( camera, highgui.CV_CAP_PROP_FRAME_HEIGHT, 480 )
-	
+
+	i = 0
 	while True:
 		try:
 			i = ( i + 1 ) % 2
@@ -32,6 +57,13 @@ if __name__ == "__main__" :
 			if frame is None:
 				print "NO FRAMEZ!?!"
 				break
+
+			x = 0
+			for row in frame:
+				for col in row:
+					x = x + 1
+					if col[0] >= options.threshold and col[1] >= options.threshold and col[2] >= options.threshold:
+						print x
 
 			if options.showgui:
 				highgui.cvShowImage( 'Camera', frame )
