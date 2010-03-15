ffc3190fbcb59d332a2a620443d68845ece24c09
John Hobbs <john@velvetcache.org>
2010-03-15 11:00:16 -0500

    Removed automatic snapshot. Added snapshot on recieving SIGUSR1.

--- generator.cpp
+++ generator.cpp
@@ -9,7 +9,7 @@
 #include <cmath>
 
 // Totally arbitrary.
-#define SAMPLE_SIZE 1024
+#define SAMPLE_SIZE 1048576
 // Almost arbitrary...
 #define THRESHOLD 215
 // I visually observed that the largest scintillations only affected a 4x4 block of pixels. So, skipping 4 rows seems safe FOR MY CAMERA
@@ -18,9 +18,10 @@
 // How consistent is your camera? Shielding might help ;-)
 #define MAX_CONSECUTIVE_DROPS 10
 // Show a progress bar every X bytes
-#define SAMPLE_PROGRESS_INTERVAL 32
+#define SAMPLE_PROGRESS_INTERVAL 1024
 
 bool run = true;
+bool snap = false;
 
 std::string get_sample_name () {
 	time_t rawtime;
@@ -33,6 +34,22 @@ std::string get_sample_name () {
 	strftime( buffer, 80, "data.%Y%m%d.%H%M%S", timeinfo );
 
 	return std::string ( buffer );
+}
+
+std::string get_snap_name ( std::string sample_name ) {
+	time_t rawtime;
+	struct tm * timeinfo;
+	char buffer [80];
+
+	time ( &rawtime );
+	timeinfo = localtime ( &rawtime );
+
+	strftime( buffer, 80, "%Y%m%d.%H%M%S", timeinfo );
+
+	sample_name.append( "-snap-" );
+	sample_name.append( buffer );
+
+	return sample_name;
 }
 
 std::string get_time_stamp () {
@@ -49,9 +66,9 @@ std::string get_time_stamp () {
 }
 
 std::string get_progress_bar ( int complete ) {
-	
+
 	int pct = floor( (float) complete / (float) SAMPLE_SIZE * 100 );
-	
+
 	std::stringstream utl;
 	utl << "[";
 	for( int i = 10; i > 0; --i ) {
@@ -61,17 +78,20 @@ std::string get_progress_bar ( int complete ) {
 			utl << "##";
 	}
 	utl << "] " << pct << "%";
-	
+
 	return utl.str();
 }
 
 void sighandler ( int sig ) {
 	std::cout << get_time_stamp() << "Caught SIG" << sig << std::endl;
-	run = false;
+	if( sig == SIGUSR1 )
+		snap = true;
+	else
+		run = false;
 }
 
 int main( int argc, char ** argv ) {
-	
+
 	char mask = (char) 1;
 	char buffer = (char) 0;
 	int counter = 0;
@@ -79,10 +99,11 @@ int main( int argc, char ** argv ) {
 		std::cout << "Uh oh! Your char size on this arch isn't a single byte. I can't fit that into my world view." << std::endl;
 		return -1;
 	}
-	
+
 	signal( SIGABRT, &sighandler );
 	signal( SIGTERM, &sighandler );
 	signal( SIGINT, &sighandler );
+	signal( SIGUSR1, &sighandler );
 
 	CvCapture* capture = cvCaptureFromCAM( CV_CAP_ANY );
 	if( ! capture ) {
@@ -94,11 +115,11 @@ int main( int argc, char ** argv ) {
 	cvSetCaptureProperty( capture, CV_CAP_PROP_FRAME_HEIGHT, 480 );
 
 	IplImage * frame;
-	
+
 	// On my camera, the first few frames are junked, so I scrap them here.
 	for( int x = 0; x <= 3; ++x )
 		frame = cvQueryFrame( capture );
-	
+
 	CvScalar pixel;
 	int row,col;
 	std::stringstream string_util;
@@ -108,15 +129,14 @@ int main( int argc, char ** argv ) {
 		sample_name = argv[1];
 	else
 		sample_name = get_sample_name();
-	
+
 	string_util << sample_name << ".bin";
 	std::ofstream file( string_util.str().c_str(), std::ios::out | std::ios::binary | std::ios::app );
 	string_util.str("");
-	
+
 	int sample_index = file.tellp();
-	bool sample_image_captured = ( 0 != sample_index );
-	
-	if( sample_image_captured )
+
+	if( 0 != sample_index )
 		std::cout << get_time_stamp() << "Restarting Existing Sample: " << sample_name << " From Byte: " << sample_index << std::endl;
 	else
 		std::cout << get_time_stamp() << "Starting New Sample: " << sample_name << std::endl;
@@ -129,22 +149,22 @@ int main( int argc, char ** argv ) {
 		if( ! frame ) {
 			std::cout << get_time_stamp() << "ERROR: Dropped a frame!" << std::endl;
 			++drop_count;
-			
+
 			if( MAX_CONSECUTIVE_DROPS <= drop_count ) {
 				std::cout << get_time_stamp() << "FATAL: Too many dropped frames." << std::endl;
 				break;
 			}
-			
+
 			continue;
 		}
-		
+
 		drop_count = 0;
 
-		if( ! sample_image_captured ) {
-			string_util << sample_name << ".png";
+		if( snap ) {
+			string_util << get_snap_name( sample_name ) << ".png";
 			if( cvSaveImage( string_util.str().c_str(), frame ) ) {
-				std::cout << get_time_stamp() << "Saved image for the sample:" << string_util.str().c_str() << std::endl;
-				sample_image_captured = true;
+				std::cout << get_time_stamp() << "Saved image:" << string_util.str().c_str() << std::endl;
+				snap = false;
 				string_util.str("");
 			}
 		}
@@ -153,43 +173,42 @@ int main( int argc, char ** argv ) {
 			for( col = 0; col < frame->width; col++ ) {
 				pixel = cvGet2D( frame, row, col );
 				if( pixel.val[0] > THRESHOLD && pixel.val[1] > THRESHOLD && pixel.val[2] > THRESHOLD ) {
-					
+
 					// Shift the LSB of the index onto the buffer
 					int index = ( row * frame->width ) + col;
 					buffer = buffer << 1;
 					buffer = buffer ^ ( ( buffer ^ (char) index ) & mask );
-					
+
 					// Full buffer?
 					if( ++counter >= 8 ) {
-					
+
 						// Write it to file immediately
 						file.write( &buffer, 1 );
 						file.flush();
-						
+
 						// Reset our buffer
 						buffer = (char) 0;
 						counter = 0;
-						
+
 						if( 0 == sample_index % SAMPLE_PROGRESS_INTERVAL && sample_index != 0 ) {
 							std::cout << get_time_stamp() << "Progress " << get_progress_bar( sample_index ) << std::endl;
 						}
-						
+
 						// Full sample?
 						if( ++sample_index >= SAMPLE_SIZE ) {
 							std::cout << get_time_stamp() << "Finished Sample: " << sample_name << std::endl;
 							file.close();
-							
+
 							sample_name = get_sample_name();
-							
+
 							string_util << sample_name << ".bin";
 							file.open( string_util.str().c_str(), std::ios::out | std::ios::binary | std::ios::app );
 							string_util.str("");
-							
+
 							std::cout  << get_time_stamp() << "Starting New Sample: " << sample_name << std::endl;
-							
+
 							row = frame->height;
 							sample_index = 0;
-							sample_image_captured = false;
 							break;
 						}
 
@@ -206,7 +225,7 @@ int main( int argc, char ** argv ) {
 	std::cout << get_time_stamp() << "Shutting Down" << std::endl;
 
 	file.close();
-	
+
 	cvReleaseCapture( &capture );
 
 	return 0;
